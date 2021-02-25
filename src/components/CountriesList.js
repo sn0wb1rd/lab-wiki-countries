cca7bf2181d84250e4e09dd34f1de0f27d17b00c
Cindy <cindytvn+git@gmail.com>
2021-02-25 17:48:33 +0100

    it 1.1 done: created components and structure

---
+++ src/components/CountriesList.js
@@ -0,0 +1,13 @@
+import React, {Component} from 'react'
+
+class CountriesList extends Component {
+    render () {
+        return (
+            <>
+                The Countries in a list
+            </>
+        )
+    }
+}
+
+export default CountriesList
---
+++ src/components/CountryDetails.js
@@ -0,0 +1,14 @@
+import React, {Component} from 'react'
+
+class CountryDetails extends Component {
+    render () {
+        return (
+            <>
+                Details of the  Countries 
+            </>
+        )
+    }
+}
+
+
+export default CountryDetails
