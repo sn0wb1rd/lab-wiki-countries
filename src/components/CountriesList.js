096b5bccaa0eb1c6c5ce37b29072e6801f63be4a
Cindy <cindytvn+git@gmail.com>
2021-02-26 08:05:32 +0100

    it 4 done. Plus the (complicated?) scrollbar..

--- src/components/CountriesList.js
+++ src/components/CountriesList.js
@@ -12,15 +12,17 @@ class CountriesList extends Component {
         // console.log('countries: ', countries[5])
 
         return (
-
-                <div className="col-5">
+                // Why does max-height in the style not work?
+                <div className="col-5 max-vh-50" style={{height: "90vh", overflow: "scroll"}}>
                     <div className="list-group">
                         {
                             countries.map((singleCountry, index) => {
+
                                 return (
-                                    <div key={index}>
-                                    <Link to={`/${singleCountry.cca3}`}>{singleCountry.flag} {singleCountry.name.common}</Link>
-                                    </div>
+                                    <Link  key={index} className="list-group-item list-group-item-action border" 
+                                            to={`/${singleCountry.cca3}`}>
+                                            {singleCountry.flag} {singleCountry.name.common}
+                                    </Link>
                                 )                        
                             })
                         }  
